b2bcde73350382dbd8309e72b70da7a3753ab99e
Kirill G. Kotenko <zelder.rus@gmail.com>
2016-03-22 11:18:08 +0300

    up mklcd

--- mktolcd.asm
+++ mktolcd.asm
@@ -1,8 +1,8 @@
 
 ; ######################################################
 ;
-;	????????? ??? MK AT89C2051 
-;	??? ?????? ? ???????? MT-12232A
+;	Программа под MK AT89C2051 
+;	для работы с дисплеем MT-12232A
 ;
 ;
 ; ZeLDER
@@ -29,7 +29,11 @@
 ;		7 -
 ;
 ; ++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+	LCD_E 	EQU P3.0
+	LCD_RW 	EQU P3.1
+	LCD_A0 	EQU P3.2
+	LCD_CS	EQU P3.3
+	LCD_RES	EQU P3.4
 
 
 ;;;;;;;;;;;;;;;;;;;;;
@@ -41,29 +45,61 @@
 ;;	R0 - data byte for LCD
 ;;	R1 - commands for LCD
 ;;	
-
+	
 
 
 ORG 0x00
 	LJMP MAIN
+	
+
+;#################################################
+;Interrupts
+
 ; TODO: interrupts
 ; TODO: timers
+
+ORG 03H ;external interrupt 0
+RETI
+ORG 0BH ;timer 0 interrupt
+RETI
+ORG 13H ;external interrupt 1
+RETI
+ORG 1BH ;timer 1 interrupt
+RETI
+ORG 23H ;serial port interrupt
+RETI
+ORG 25H ;locate beginning of rest of program
+
+
+
+;#################################################
+ORG 0x32 ;0x0A
+;; INIT
+INITIALIZE: ;set up control registers & ports
+	MOV TCON, #0x00
+	MOV TMOD, #0x00
+	MOV PSW, #0x00 
+	MOV IE, #0x00 ;disable interrupts
+	RET
+
 	
 ; =====================
 ;
-; ???????? ?????????
+; Точка входа
 ;
 ; =====================
-ORG 0x0A
 MAIN:
+	ACALL INITIALIZE
 	ACALL LCDINIT
 	ACALL LCDCLEAR
 	ACALL MAINLOOP
 	
 	
 MAINLOOP:
+	ACALL LCDDRAW ; Draw once
+	
 	MOV R5, #10	; cycle
-	ACALL LCDDRAW
+	;ACALL LCDDRAW - Do nothing
 	DJNZ R5, MAINLOOP
 	RET
 	
@@ -74,24 +110,18 @@ MAINLOOP:
 ;
 ; =====================
 DELAYMS:	; A = times
-	MOV R6, #230
-	LX:
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		DJNZ R6, LX
-	MOV R7, #230
-	LB:
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		NOP
-		DJNZ R7, LB
+	MOV R7, A
+	LMX:
+		MOV R6, #230
+		LX:
+			NOP
+			NOP
+			NOP
+			NOP
+			NOP
+			NOP
+			DJNZ R6, LX
+		DJNZ R7, LMX
 	RET
 DELAYNS:	; A = times
 	; TODO
@@ -112,9 +142,11 @@ DELAYUS:	; A = times
 ; =====================
 
 ; ----------------------	
-; ????????????? ???????
+; Инициализация дисплея
 ; ----------------------
 LCDINIT:
+	MOV A, #20
+	ACALL DELAYMS
 	;s_mtE(1)
 	;s_mtRES(0)
 	MOV P3, #0x01 ;#0b00000001 ;(E=1, RW=0, A0=0, CS=0, RES=0)
@@ -122,7 +154,7 @@ LCDINIT:
 	MOV A, #12
 	ACALL DELAYUS
 	;s_mtRES(1)
-	SETB P3.4
+	SETB LCD_RES ;P3.4
 	;s_delayMs(2.5)	#>1
 	MOV A, #2
 	ACALL DELAYMS
@@ -158,7 +190,7 @@ LCDINIT:
 
 
 ; ----------------------	
-; ?????? ???????/?????? ? ???????
+; Подача кода/команды в дисплей
 ; ----------------------
 LCDWRITE:  ; R0(A) = data byte, R1 = cmd
 	;s_writeByte(b, cd, lr)
@@ -174,12 +206,12 @@ LCDWRITE:  ; R0(A) = data byte, R1 = cmd
 	MOV A, #40
 	ACALL DELAYNS
 	;s_mtE(0)
-	CLR P3.0
+	CLR LCD_E
 	;s_delayNs(160.0)	#>160
 	MOV A, #160
 	ACALL DELAYNS
 	;s_mtE(1)
-	SETB P3.0
+	SETB LCD_E
 	;s_delayNs(200.0 - 40.0 - 160.0) #2000.0 - 40.0 - 160.0
 	MOV A, #10
 	ACALL DELAYNS
@@ -207,7 +239,7 @@ LCDWRITE_DATA_R:	; R0 = data byte
 	
 	
 ; ----------------------	
-; ??????? ???????
+; Очистка дисплея
 ; ----------------------
 LCDCLEAR:
 	MOV R4, #4	; page cycle
@@ -226,7 +258,7 @@ LCDCLEAR:
 		MOV R0, #0x13
 		ACALL LCDWRITE_CODE_L
 		; left draw
-		MOV R0, #0x00 ; clear symbol
+		MOV R0, #0x01 ; clear symbol
 		MOV R3, #61	; row cycle
 		LCDCLEAR_PAGE_LEFT:
 			MOV A, R3
@@ -242,7 +274,7 @@ LCDCLEAR:
 		MOV R0, #0x00
 		ACALL LCDWRITE_CODE_R
 		; right draw
-		MOV R0, #0x00 ; clear symbol
+		MOV R0, #0x01 ; clear symbol
 		MOV R3, #61	; row cycle
 		LCDCLEAR_PAGE_RIGHT:
 			MOV A, R3
@@ -256,7 +288,7 @@ LCDCLEAR:
 	RET
 	
 ; ----------------------	
-; ????? ?? ???????
+; Рисуем в дисплей
 ; ----------------------
 LCDDRAW:
 	ACALL LCDCLEAR
